a6b2e75d3543d7e21209b7e7a925e5c2c3c05f8c
Abel <malebochester@gmail.com>
2025-12-19 23:50:15 +0200

    delete files

--- src/pages/modules/404NotFound/404.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound(container) {
-  container.innerHTML = `
-    <div class="card">
-      <h3>404</h3>
-      <p>Page not found.</p>
-    </div>
-  `;
-}
--- src/pages/modules/login.js
+++
@@ -1,8 +0,0 @@
-export default function Login(container) {
-	container.innerHTML = `
-		<section class="section">
-			<h2>Login</h2>
-			<p class="placeholder">Use the login modal (click the avatar or New Note).</p>
-		</section>
-	`;
-}
